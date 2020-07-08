f2887aee50da1f0f4802a431a223b0fdf55aeeb5
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-07-08 23:20:19 +0100

    Multidimensional Array

---
+++ MultidimensionalArrays/src/com/dodemy/Main.java
@@ -0,0 +1,11 @@
+package com.dodemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Two dimensional array: int[][]");
+        TwoDimensional.TwoDim();
+        System.out.println("Looping through two dimensional array");
+        LoopingThroughTwoDimArray.TwoDimArrayLooping();
+    }
+}
---
+++ MultidimensionalArrays/src/com/dodemy/TwoDimensional.java
@@ -0,0 +1,14 @@
+package com.dodemy;
+
+/*
+To access the elements of the myNumbers array, specify two indexes: one for the array,
+and one for the element inside that array. This example accesses the third element (2)
+in the second array (1) of myNumbers:
+ */
+public class TwoDimensional {
+    public static void TwoDim() {
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        int x = myNumbers[1][2];
+        System.out.println(x);
+    }
+}
