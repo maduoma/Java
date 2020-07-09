bd138cab436ca205008f918f4bd1cd844c4078f1
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-07-09 02:29:06 +0100

    Date And Time Formatting

---
+++ DateFormatting/src/com/dodemy/DateAndTime.java
@@ -0,0 +1,15 @@
+package com.dodemy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+    public static void showDateAndTime() {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);
+    }
+}
---
+++ DateFormatting/src/com/dodemy/Main.java
@@ -0,0 +1,8 @@
+package com.dodemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    DateAndTime.showDateAndTime();
+    }
+}
