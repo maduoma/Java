9e066a8e32311c8719deb14b0dc8e5913a2ca78d
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-07 16:28:52 +0100

    Sort method

--- MultidimensionalArrays/src/com/dodemy/SimpleArray.java
+++ MultidimensionalArrays/src/com/dodemy/SimpleArray.java
@@ -7,6 +7,7 @@ public class SimpleArray {
     public void SimpleDimArray(){
         int[] numbers = {1, 2, 3, 4};
         System.out.println(numbers.length);
+        Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
 
