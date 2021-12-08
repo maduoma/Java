f74f9165e5ae3c8972500cf8cc097573669fd13c
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-08 09:58:07 +0100

    Mortgage Calculator: Monthly Repayment

--- MortgageCalculator/src/com/dodemy/MortgageCalculator.java
+++ MortgageCalculator/src/com/dodemy/MortgageCalculator.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
 
+    public static final byte PERCENT = 100;
+    public static final byte MONTHS_IN_YEAR = 12;
+
     protected void CalculateMortgage() {
         Scanner scanner = new Scanner(System.in);
         //M = P[r(1 + r)power n  / (1 + r) power n - 1]
         System.out.print("Principal: ");
-        double principal = scanner.nextDouble();
+        int principal = scanner.nextInt();
         System.out.print("Annual Interest Rate: ");
-        double monthlyRate = scanner.nextDouble();
-        monthlyRate = (monthlyRate / 100) / 12;
+        float monthlyRate = scanner.nextFloat();
+        monthlyRate = (monthlyRate / PERCENT) / MONTHS_IN_YEAR;
         System.out.print("Period in years: ");
-        byte period = scanner.nextByte();
-        int numberOfRepayment = period * 12;
+        byte years = scanner.nextByte();
+        int numberOfRepayment = years * MONTHS_IN_YEAR;
         double mortgage = principal * (monthlyRate * Math.pow((1 + monthlyRate), numberOfRepayment)) / (Math.pow(1 + monthlyRate, numberOfRepayment) - 1 );
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
 
