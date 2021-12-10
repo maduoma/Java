5a06c89e4f6a79a58d0d0ac5b38bfdc6374fb0b0
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-10 09:32:12 +0100

    Mortgage Calculator: Monthly Repayment - Data Validation

--- MortgageCalculator/src/com/dodemy/Main.java
+++ MortgageCalculator/src/com/dodemy/Main.java
@@ -1,7 +1,6 @@
 package com.dodemy;
 
 public class Main {
-    // No Data validation here
     public static void main(String[] args) {
         MortgageCalculator mortgageCalculator = new MortgageCalculator();
         mortgageCalculator.CalculateMortgage();
