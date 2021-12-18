22c63b97babf7629bdfbbb9cf628c00fa2992247
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-18 09:07:51 +0100

    OOP Association: Here we Associate Vehicle and Its Driver

---
+++ OOP_IN_JAVA/Association/Main/src/com/dodemy/Main.java
@@ -0,0 +1,22 @@
+package com.dodemy;
+// Association is a common term used for both has-a and part-of relationship model, though it is not limited to the 2 r/ships, we are not worried about the lifetime dependency between the objects.
+// The objects can exist differently or independently.
+//OOP Association in Java: The child and parent objects have their own lifecycles and there's no owner.
+class Vehicle {
+    // Complete this implementation
+
+}
+
+class Driver {
+    // Complete this implementation
+
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+
+    }
+
+}
