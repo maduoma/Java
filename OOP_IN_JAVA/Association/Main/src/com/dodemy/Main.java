180e5652b0eaeb7e13aabe8e827d962f085ec3ea
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-18 09:23:19 +0100

    OOP Association: Here we Associate Vehicle and Its Driver

--- OOP_IN_JAVA/Association/Main/src/com/dodemy/Main.java
+++ OOP_IN_JAVA/Association/Main/src/com/dodemy/Main.java
@@ -4,11 +4,25 @@ package com.dodemy;
 //OOP Association in Java: The child and parent objects have their own lifecycles and there's no owner.
 class Vehicle {
     // Complete this implementation
+    int id;
+    String model;
+
+    public Vehicle(int id, String model) {
+        this.id = id;
+        this.model = model;
+    }
 
 }
 
 class Driver {
     // Complete this implementation
+    String driverName;
+    Vehicle vehicle;
+
+    public Driver(String driverName, Vehicle vehicle) {
+        this.driverName = driverName;
+        this.vehicle = vehicle;
+    }
 
 }
 
@@ -16,7 +30,17 @@ class Main {
 
     public static void main(String[] args) {
         // Write your code here
-
+        Vehicle vehicle = new Vehicle(4453, "Volvo");
+        {
+            Driver  driver = new Driver("john", vehicle);
+            System.out.println(driver.driverName +
+                    " is the owner of car Id: " + driver.vehicle.id);
+        }
+        System.out.println();
+        System.out.println("Vehicle exists outside Driver block ie {}");
+        System.out.println("This is to show that Vehicle exists on its own with Driver!");
+        System.out.println("Vehicle Id: " +vehicle.id);
+        System.out.println("Vehicle model: " +vehicle.model);
     }
 
 }
