9e0c74216bbd23e633e29e6857c39191b124b30b
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-19 15:13:53 +0100

    Class in Java

---
+++ OOP_IN_JAVA/Classes/Class/src/com/dodemy/Main.java
@@ -0,0 +1,32 @@
+package com.dodemy;
+
+//The Structure of a Java Class
+class Car { // Class name
+
+    // Class Data members
+    int topSpeed;
+    int totalSeats;
+    int fuelCapacity;
+    String manufacturer;
+
+    // Class Methods
+    void refuel() {
+
+    }
+
+    void park() {
+
+    }
+
+    void drive() {
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ OOP_IN_JAVA/Classes/Class/src/crime/Thief.java
@@ -0,0 +1,9 @@
+package crime;
+import justice.*;
+
+class Thief {
+    public static void main(String[] args){
+        Cop obj = new Cop();
+        //obj.fire(); //Compile Time Error
+    }
+}
