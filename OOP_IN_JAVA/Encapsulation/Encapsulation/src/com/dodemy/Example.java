229734b5ef022ccd56feb4b1b4a8f84da2a81b1d
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-20 05:40:48 +0100

    Encapsulation in Java

---
+++ OOP_IN_JAVA/Encapsulation/Encapsulation/src/com/dodemy/Example.java
@@ -0,0 +1,24 @@
+package com.dodemy;
+//Not part of encapsulation
+class Example {
+    static int i = 10;
+    static String s = "static";
+
+    static void display() {
+        System.out.println("i = " + i);
+        System.out.println("s = " + s);
+    }
+
+    void func() {
+        display();
+    }
+
+
+    public static void main(String[] args) {
+        Example obj = new Example();
+
+        obj.func();
+        display();
+    }
+
+}
---
+++ OOP_IN_JAVA/Encapsulation/Encapsulation/src/com/dodemy/Patient.java
@@ -0,0 +1,35 @@
+package com.dodemy;
+
+class Patient {
+
+    //Declare member variables here
+    private String name;
+    private int id;
+    private String doctor;
+
+    //Declare Getters and Setters here
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+}
